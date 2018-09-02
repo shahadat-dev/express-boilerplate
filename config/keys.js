868cc297d05b2d13626d48c36d33f3d9ef198d09
Hakao <hakaopublik@gmail.com>
2018-09-02 12:38:26 +0600

    User Registration Postman

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://mati:mati1986@ds135747.mlab.com:35747/devconnector"
+  mongoURI: "mongodb://mati:mati1986@ds135747.mlab.com:35747/devconnector",
+  localMongoDB: "mongodb://localhost:27017/devconnector"
 };
