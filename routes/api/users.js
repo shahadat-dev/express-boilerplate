8ef11e1c9a96dfdfa33cdd0d9202a98d39912d45
Hakao <hakaopublik@gmail.com>
2018-09-02 13:24:46 +0600

    Email Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,15 +9,21 @@ const User = require("../../models/User");
 // @route   GET /api/users/test
 // @desc    Test users route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+router.get("/test", (req, res) => res.json({
+  msg: "Users works"
+}));
 
 // @route   POST /api/users/register
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      return res.status(400).json({
+        email: "Email already exists"
+      });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size
@@ -44,6 +50,41 @@ router.post("/register", (req, res) => {
       });
     }
   });
+});
+
+// @route   POST /api/users/login
+// @desc    Login User / Return JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        return res.status(404).json({
+          email: "User not found."
+        })
+      }
+
+      // Check Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({
+              msg: "Success"
+            });
+          } else {
+            res.status(400).json({
+              password: "Password incorrect"
+            });
+          }
+        })
+    })
 });
 
 module.exports = router;
