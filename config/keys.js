21c3ac2b35db9c59d6ee52ea171a3e02b8406baa
Hakao <hakaopublik@gmail.com>
2018-09-02 13:46:45 +0600

    Creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://mati:mati1986@ds135747.mlab.com:35747/devconnector",
-  localMongoDB: "mongodb://localhost:27017/devconnector"
+  localMongoDB: "mongodb://localhost:27017/devconnector",
+  secretOrKey: "secret"
 };
