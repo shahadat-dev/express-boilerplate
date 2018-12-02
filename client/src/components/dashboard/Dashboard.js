e6e86545c05bcd4184d2763b773b1224012a98aa
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-03 02:07:47 +0600

    spinner added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import { getCurrentProfile } from '../../actions/profileActions'
+import Spinner from '../common/Spinner'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -13,9 +15,37 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { user } = this.props.auth
+    const { profile, loading } = this.props.profile
+
+    let dashboardContent
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        )
+      }
+    }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="display-4">Dashboard</h1>
+            {dashboardContent}
+          </div>
+        </div>
       </div>
     )
   }
@@ -24,12 +54,14 @@ class Dashboard extends Component {
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  errors: state.errors
+  errors: state.errors,
+  profile: state.profile
 })
 
 export default connect(
