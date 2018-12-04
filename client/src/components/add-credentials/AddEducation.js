6a2754d2a0859228a66d47851253b190fdaff05c
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-04 18:19:40 +0600

    add experience

---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import TextFieldGroup from '../common/TextFieldGroup'
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup'
+import InputGroup from '../common/InputGroup'
+import SelectListGroup from '../common/SelectListGroup'
+
+class AddEducation extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default AddEducation
