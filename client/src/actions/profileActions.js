2f7ac948e17b5157997cfdbaeb52519bc1d2437d
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-04 18:49:32 +0600

    Add education

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -54,6 +54,19 @@ export const addExperience = (expData, history) => dispatch => {
     )
 }
 
+// Add Education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post('/api/profile/education', eduData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can not be undone!')) {
--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -1,16 +1,174 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import TextFieldGroup from '../common/TextFieldGroup'
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup'
-import InputGroup from '../common/InputGroup'
-import SelectListGroup from '../common/SelectListGroup'
+import { addEducation } from '../../actions/profileActions'
 
 class AddEducation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      school: '',
+      degree: '',
+      fieldOfStudy: '',
+      from: '',
+      to: '',
+      current: false,
+      description: '',
+      errors: {},
+      disabled: false
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onCheck = this.onCheck.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors })
+    }
+  }
+
+  onCheck(e) {
+    this.setState({
+      disabled: !this.state.disabled,
+      current: !this.state.current
+    })
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const eduData = {
+      school: this.state.school,
+      degree: this.state.degree,
+      fieldOfStudy: this.state.fieldOfStudy,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    }
+
+    this.props.addEducation(eduData, this.props.history)
+  }
+
   render() {
-    return <div />
+    const { errors } = this.state
+    return (
+      <div className="add-experience">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
+              <h1 className="display-4 text-center">Add Education</h1>
+              <p className="lead text-center">
+                Add any school, bootcamp, etc that you have attended
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+
+              <form onSubmit={this.onSubmit} noValidate>
+                <TextFieldGroup
+                  placeholder="* School"
+                  name="school"
+                  value={this.state.school}
+                  onChange={this.onChange}
+                  error={errors.school}
+                />
+
+                <TextFieldGroup
+                  placeholder="* Degree or Certification"
+                  name="degree"
+                  value={this.state.degree}
+                  onChange={this.onChange}
+                  error={errors.degree}
+                />
+
+                <TextFieldGroup
+                  placeholder="* Field of Study"
+                  name="fieldOfStudy"
+                  value={this.state.fieldOfStudy}
+                  onChange={this.onChange}
+                  error={errors.fieldOfStudy}
+                />
+
+                <h6>From Date</h6>
+                <TextFieldGroup
+                  name="from"
+                  type="date"
+                  value={this.state.from}
+                  onChange={this.onChange}
+                  error={errors.from}
+                />
+
+                <h6>To Date</h6>
+                <TextFieldGroup
+                  name="to"
+                  type="date"
+                  value={this.state.to}
+                  onChange={this.onChange}
+                  error={errors.to}
+                  disabled={this.state.disabled ? 'disabled' : ''}
+                />
+
+                <div className="form-check mb-4">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    name="current"
+                    value={this.state.current}
+                    checked={this.state.current}
+                    onChange={this.onCheck}
+                    id="current"
+                  />
+                  <label htmlFor="current" className="form-check-label">
+                    Current
+                  </label>
+                </div>
+
+                <TextAreaFieldGroup
+                  placeholder="Program Description"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.onChange}
+                  error={errors.description}
+                  info="Tell us about the program that you were in"
+                />
+
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-export default AddEducation
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+})
+
+export default connect(
+  mapStateToProps,
+  { addEducation }
+)(withRouter(AddEducation))
