2ef6d57b9227e63741223fbdf5e90d968e37ad9f
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-06 20:00:58 +0600

    show all profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -6,7 +6,8 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from './types'
 
 // Get current profile
@@ -63,6 +64,25 @@ export const addEducation = (eduData, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    )
+}
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading())
+  axios
+    .get('/api/profile/all')
+    .then(res => {
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     )
 }
